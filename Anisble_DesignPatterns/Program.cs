7e733466de89dc5159a95ef364e790680019888f
Karthik Elangovan <karthik.ellangovan@gmail.com>
2018-03-10 20:53:45 +1100

    commit
    q

--- Anisble_DesignPatterns/Program.cs
+++ Anisble_DesignPatterns/Program.cs
@@ -10,6 +10,25 @@ namespace Anisble_DesignPatterns
     {
         static void Main(string[] args)
         {
-        }
+
+            AnisbleSingleton.SingleInstance.currentText = "From Prg";
+            AnisbleSingleton.SingleInstance.Print();
+            Console.ReadLine();
+
+
+            AnisbleSingleton.SingleInstance.Print();
+            Console.ReadLine();
+
+            AnisbleSingleton.SingleInstance.Print("First");
+            Console.ReadLine();
+
+            AnisbleSingleton.SingleInstance.Print("Second");
+            Console.ReadLine();
+
+            AnisbleSingleton.SingleInstance.Print();
+            Console.ReadLine();
+
+             
+    }
     }
 }
